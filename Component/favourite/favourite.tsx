c4430150c7502828d758d8cd39f3dec7be874aa0
Connectgauravkaushik <101702222+Connectgauravkaushik@users.noreply.github.com>
2023-03-01 14:04:32 +0530

    Update favourite.tsx

--- Component/favourite/favourite.tsx
+++ Component/favourite/favourite.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import data from '../data.json';
+
+  const posterStyle = {
+     height:'400px',
+     widht:'250px'
+  }
+  
 function Favorite() {
   const Data = data.favourite;  
   return (
@@ -8,7 +14,7 @@ function Favorite() {
       <div className='container'> 
           {Data.map((data) => (
             <div style={{ float:'left',padding:'1em' ,textAlign:'center' }} > 
-             <img className='small'  src={data.posterurl} alt="Avatar"></img>
+             <img className='small'  src={data.posterurl} alt="Avatar" style={{ ...posterStyle }}></img>
                <h2>{data.title}</h2>
                <p>Add to favorite</p>
             </div>
