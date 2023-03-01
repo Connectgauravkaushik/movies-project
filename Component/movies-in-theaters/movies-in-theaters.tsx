6dcf4ae7c9da0db430ef478700c4dd502947f290
Connectgauravkaushik <101702222+Connectgauravkaushik@users.noreply.github.com>
2023-03-01 14:05:05 +0530

    Update movies-in-theaters.tsx

--- Component/movies-in-theaters/movies-in-theaters.tsx
+++ Component/movies-in-theaters/movies-in-theaters.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import data from '../data.json';
 
+const posterStyle = {
+  height: '400px',
+  widht: '250px'
+}
+
 function Intheatre() {
     const DataTheatre = data['movies-in-theaters'];
     return (
@@ -9,7 +14,7 @@ function Intheatre() {
                 <div className='container'>
                     {DataTheatre.map((data) => (
                         <div style={{ float: 'left', padding: '1em', textAlign: 'center' }} >
-                            <img className='small' src={data.posterurl} alt="Avatar"></img>
+                            <img className='small' src={data.posterurl} alt="Avatar" style={{ ...posterStyle }}></img>
                             <h2>{data.title}</h2>
                             <p>Add to favorite</p>
                         </div>
